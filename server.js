21cd08ff9858b9eb51ce3116a65585398f52e366
02madmax <maxlamthach10@gmail.com>
2023-09-09 16:15:08 -0600

    added starter code and server.js file

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname + 'app/public')));
+
+require('./app/routing/apiRoutes.js')(app);
+
+require('./app/routing/htmlRoutes.js')(app);
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
